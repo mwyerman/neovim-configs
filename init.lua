5513652d97bfa8089915aa3461611151ff90ab37
Max Wyerman <mwyerman@gmail.com>
2021-08-20 22:53:39 -0400

    Install lualine as a statusline
    Closes #2.

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
 require('plugins')
+
+require('lualine').setup()
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,10 @@
 return require('packer').startup(function()
 	-- Packer (package manager)
 	use 'wbthomason/packer.nvim'
+
+	-- lualine (statusline)
+	use {
+		'hoob3rt/lualine.nvim',
+		requires = {'kyazdani42/nvim-web-devicons', opt = true}
+	}
 end)
