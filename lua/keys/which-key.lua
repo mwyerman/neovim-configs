cde581999a16030a2ba9a9d63944d6a0d824a965
Max Wyerman <mwyerman@gmail.com>
2021-08-21 01:33:50 -0400

    Add packer commands to which-key (#5)

---
+++ lua/keys/which-key.lua
@@ -0,0 +1,13 @@
+local wk = require('which-key')
+
+wk.register({
+    p = {
+        name = "packer",
+        i = { ":PackerInstall<cr>", "install" },
+        s = { ":PackerSync<cr>", "sync" },
+        S = { ":PackerStatus<cr>", "status" },
+        u = { ":PackerUpdate<cr>", "update" },
+        c = { ":PackerClean<cr>", "clean" },
+        C = { ":PackerCompile<cr>", "compile" },
+    }
+}, { prefix = "<leader>" })
