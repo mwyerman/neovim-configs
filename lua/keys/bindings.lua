460045b6661b79a04be090ae8a366242582561ba
Max Wyerman <mwyerman@gmail.com>
2021-08-21 01:33:25 -0400

    Add some keybindings

---
+++ lua/keys/bindings.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.api.nvim_set_keymap('n', '<c-j>', '\\<C-n>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<c-k>', '\\<C-p>', { noremap = true })
+
+-- Better line indentation
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+
+-- Better window navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+
+-- jk or kj to leave insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
+
