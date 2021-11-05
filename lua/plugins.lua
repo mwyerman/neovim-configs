3949529c5cfbe1bef25082c441cfb5be49ace4e0
Max Wyerman <mwyerman@frederickscompany.com>
2021-11-05 15:27:41 -0400

    Update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,22 +3,47 @@ return require("packer").startup(function()
 	use("wbthomason/packer.nvim")
 
 	-- themes
-	use("christianchiarulli/nvcode-color-schemes.vim")
-	use("nvim-treesitter/nvim-treesitter")
+	use({ "nvim-treesitter/nvim-treesitter" })
+
+	use({ "christianchiarulli/nvcode-color-schemes.vim", module = "nvcode-color-schemes.vim" })
+
+	-- telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+		config = function()
+			require("dev/telescope")
+		end,
+	})
+
+	-- telescope fuzzy finder
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "make",
+	})
 
 	-- lualine (statusline)
 	use({
 		"hoob3rt/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("ui/lualine")
+		end,
 	})
 
 	-- luatab (tabs)
-	use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
+	use({
+		"akinsho/bufferline.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("ui/tabline")
+		end,
+	})
 
 	-- bufdelete (smarter buffer delete)
 	use("famiu/bufdelete.nvim")
 
-	-- commnent.nvim
+	-- comment.nvim
 	use({
 		"numToStr/Comment.nvim",
 		config = function()
@@ -29,46 +54,28 @@ return require("packer").startup(function()
 	-- auto pairs (auto close brackets)
 	use("jiangmiao/auto-pairs")
 
-	-- cursor line (highlight selection)
-	use("yamatsum/nvim-cursorline")
+	-- autotag (auto html/xml tags)
+	use("windwp/nvim-ts-autotag")
 
-	-- vinegar (file navigation)
-	use("jeetsukumaran/vim-filebeagle")
+	-- cursor line (highlight selection)
+	use({ "yamatsum/nvim-cursorline", module = "nvim-cursorline" })
+
+	-- file beagle (file navigation)
+	use({ "jeetsukumaran/vim-filebeagle", keys = "-" })
 
 	-- which-key (keybindings)
 	use({
 		"folke/which-key.nvim",
 		config = function()
-			require("which-key").setup({})
+			require("keys/which-key")
 		end,
 	})
 
 	-- neovim lsp (language server protocol)
 	use("neovim/nvim-lspconfig")
 
-	-- lsp autocomplete
-	use({
-		"hrsh7th/nvim-compe",
-		requires = {
-			{ "hrsh7th/vim-vsnip" },
-			{ "hrsh7th/vim-vsnip-integ", opt = true },
-		},
-	})
-
-	-- telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-
-	-- telescope fuzzy finder
-	use({
-		"nvim-telescope/telescope-fzf-native.nvim",
-		run = "make",
-	})
-
 	-- git blame
-	use("f-person/git-blame.nvim")
+	use({ "f-person/git-blame.nvim", module = "git-blame.nvim" })
 
 	-- git signs
 	use({
@@ -86,29 +93,41 @@ return require("packer").startup(function()
 	-- lazygit
 	use("kdheepak/lazygit.nvim")
 
-	-- vim-doge (docstring generator)
-	use({
-		"kkoomen/vim-doge",
-		run = ":call doge#install()",
-	})
-
 	-- hop (jump to)
 	use({
 		"phaazon/hop.nvim",
 		as = "hop",
 		config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
 			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
 		end,
+		keys = "<Leader>h",
 	})
 
+	-- Neogit (alternative git interface)
 	use({
 		"TimUntersberger/neogit",
 		requires = "nvim-lua/plenary.nvim",
+		cmd = "Neogit",
 	})
 
-    -- git fugitive (:Git command)
-    use({
-        "doronbehar/nvim-fugitive",
-    })
+	-- git fugitive (:Git command)
+	use({
+		"doronbehar/nvim-fugitive",
+	})
+
+	-- startup time profiler
+	use("tweekmonster/startuptime.vim")
+
+	-- lsp autocomplete
+	use({
+		"hrsh7th/nvim-compe",
+		requires = {
+			{ "hrsh7th/vim-vsnip" },
+			{ "hrsh7th/vim-vsnip-integ", opt = true },
+		},
+		event = "InsertEnter",
+		config = function()
+			require("dev/compe")
+		end,
+	})
 end)
