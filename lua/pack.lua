7e583750baf97bccfe4329b1168443e6033175b3
Max Wyerman <mwyerman@gmail.com>
2021-12-01 20:07:56 -0500

    Install lualine statusbar

--- lua/pack.lua
+++ lua/pack.lua
@@ -1,15 +1,25 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
+
     use({
 	'folke/which-key.nvim',
 	config = function()
 	    require('plugins/whichkey')
 	end,
     })
+
     use({
 	'folke/tokyonight.nvim',
 	config = function()
 	    require('plugins/themes/tokyonight')
-	end
+	end,
+    })
+
+    use({
+	'nvim-lualine/lualine.nvim',
+	requires = {'kyazdani42/nvim-web-devicons', opt = true},
+	config = function()
+	    require('plugins/lualine')
+	end,
     })
 end)
