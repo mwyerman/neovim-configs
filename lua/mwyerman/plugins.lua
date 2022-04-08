d2b1a3e574b706105a0bba2f8572dcc624904c09
Max Wyerman <mwyerman@gmail.com>
2022-04-08 00:40:43 -0400

    Add comment support

--- lua/mwyerman/plugins.lua
+++ lua/mwyerman/plugins.lua
@@ -45,6 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- auto pair completion
+  use "numToStr/Comment.nvim" -- commenting support
 
   -- tokyo night colorscheme
   use "folke/tokyonight.nvim"
@@ -73,6 +74,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use "JoosepAlviste/nvim-ts-context-commentstring" -- comment.nvim treesitter support
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
