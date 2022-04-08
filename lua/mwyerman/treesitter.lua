fa2a744472b553bd400573536c41d36775313aee
Max Wyerman <mwyerman@gmail.com>
2022-04-07 22:35:31 -0400

    Add treesitter

---
+++ lua/mwyerman/treesitter.lua
@@ -0,0 +1,16 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+  return
+end
+
+configs.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained", or a list of languages
+  sync_install = false, -- install languages syncronously
+  ignore_install = { "" }, -- list of parsers to ignore installing
+  highlight = {
+    enable = true,
+    disable = { "" }, -- languages to disable
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true, disable = { "yaml" } },
+}
