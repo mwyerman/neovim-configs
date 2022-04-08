76f4bfe5dd44b515d8ed6f97c192db12079a9c4f
Max Wyerman <mwyerman@gmail.com>
2022-04-07 22:16:23 -0400

    Remove esc keymap for cmp

--- lua/mwyerman/cmp.lua
+++ lua/mwyerman/cmp.lua
@@ -62,10 +62,6 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
-    ["<ESC>"] = cmp.mapping {
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
